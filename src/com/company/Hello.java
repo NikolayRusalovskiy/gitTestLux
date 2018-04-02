b10393bc19e23bed1ad3471c6acf87a04d6fbf01
Nikolay <nikolay@ukr.net>
2018-04-02 15:12:13 +0300

    add Hello java

---
+++ src/com/company/Hello.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by grey36 on 02.04.2018.
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Luxoft!");
+		System.out.println("dasda");
+	}
+
+}
